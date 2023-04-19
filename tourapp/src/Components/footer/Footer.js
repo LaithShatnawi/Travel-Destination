91d169b5eb4fd18ef9c95a6d2d47e77ebe099262
LaithShatnawi <laith.sh66@gmail.com>
2023-04-19 12:27:25 +0000

    creating new react app

---
+++ tourapp/src/Components/footer/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <>
+        <p>
+            <ul>
+                <li><a href='https://www.facebook.com/' target='_blank' rel="noreferrer noopener">Facebook</a></li>
+                <li><a href='https://www.linkedin.com/in/laith-shatnawi-556630146/' target='_blank' rel="noreferrer noopener">linkedIn</a></li>
+            </ul>
+        </p>
+        <p>
+            Author: Laith Shatnawi
+        </p>
+        </>
+    )
+}
+
+export default Footer;
